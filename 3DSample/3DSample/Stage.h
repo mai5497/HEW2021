e9c2e0e405307d6ea924d800c9dc7a28d2082d8d
itida <maimai130806@gmail.com>
2021-12-26 06:43:37 +0900

    フィールドモデル読込一回消した

--- 3DSample/3DSample/Stage.h
+++ 3DSample/3DSample/Stage.h
@@ -1,29 +1,11 @@
 #ifndef __STAGE_H__
 #define __STAGE_H__
 
-//========================= インクルード部 ===========================
-#include "DwarfBase.h"
-#include "FBX/FBXPlayer.h"
 #include "FieldManager.h"
 
-//========================= 定数・マクロ定義 ===========================
-enum MODELNUM {
-	LOWESTGROUND = 0,
-	DWARFGROUND,
-	PLAYERGROUND_R,
-	PLAYERGROUND_L,
-	PLAYERGROUND_INSIDE,
-	PLAYERGROUND_BEFORE,
-
-	MODEL_MAX
-};
-
-
-//========================= クラス定義 ===========================
 class Stage
 {
 public:
-	//---関数
 	Stage();
 	~Stage();
 
@@ -35,18 +17,10 @@ public:
 	FieldManager *GetField(int index);
 	int GetFieldNum();
 
-	//---変数
-	ID3D11ShaderResourceView *m_pStageTex[MODEL_MAX];
-
 private:
-	//---関数
-	bool LoadStage(const char *pFilePath,int num);
-
-	//---変数
 	FieldManager **m_ppFields;
 	int m_nFieldNum;
-	static DrawBuffer *m_pBuffer[MODEL_MAX];
-	static FBXPlayer *m_pFBX[MODEL_MAX];		// FBXファイル操作クラス
+
 };
 
 
